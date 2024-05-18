2aabfeae205437f252140456b380903d9e110eb2
matsuo <kaeru0343@gmail.com>
2024-05-18 17:54:33 +0900

    fetchthreads

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <>
-      <div>テスト</div>
-    </>
+export const App = () => {
+  const [threadData, setThreadData] = useState<{ id: string; title: string }[]>(
+    []
   );
-}
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          "https://railway.bulletinboard.techtrain.dev/threads?offset=0"
+        );
+        const data = await response.json();
+        setThreadData(data);
+      } catch (error) {
+        console.error("取得できません", error);
+      }
+    };
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <h2>新着スレッド</h2>
+      {threadData.map((threadData) => (
+        <div key={threadData.id}>
+          <p>
+            id: {threadData.id} title: {threadData.title}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
