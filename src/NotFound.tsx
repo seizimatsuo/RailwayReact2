9c04de151d3da45b94a59ac2c2c1433212ca2708
matsuo <kaeru0343@gmail.com>
2024-05-23 11:32:30 +0900

    Layout

---
+++ src/NotFound.tsx
@@ -0,0 +1,10 @@
+import { Container, Typography } from "@mui/material";
+
+const NotFound = () => {
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h3">ページが見つかりません</Typography>
+    </Container>
+  );
+};
+export default NotFound;
